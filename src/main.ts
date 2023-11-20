4277ee73cf2fee8607841ea516e0038732825448
voitd <dimytr.voitenko@gmail.com>
2023-11-20 22:43:27 +0200

    style: added theme

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,38 @@ import router from "./router";
 
 const pinia = createPinia();
 
+const light = {
+  dark: false,
+  colors: {
+    primary: "#667acc",
+    secondary: "#FFFFFF",
+    background: "#F5F5F5",
+    foreground: "#F5F5F5",
+    "v-theme-background": "#FFFFFF",
+    "v-theme-card": "#FFFFFF",
+  },
+};
+
+const dark = {
+  dark: true,
+  colors: {
+    primary: "#667acc",
+    secondary: "#1E2127",
+    background: "#18191B",
+    foreground: "#1E1E1E",
+    "v-theme-background": "#1E2127",
+    "v-theme-card": "#22252B",
+  },
+};
+
+const darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
 const vuetify = createVuetify({
   theme: {
-    defaultTheme: "dark",
+    defaultTheme: darkMode ? "dark" : "light",
+    themes: {
+      dark,
+      light,
+    },
   },
 });
 
